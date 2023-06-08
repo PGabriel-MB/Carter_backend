b463f1290c1b31359488165fc121e13a5d46f82a
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-06-07 21:02:48 -0300

    consertada rota de criação de empresa

--- authentication/serializers/company.py
+++ authentication/serializers/company.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from authentication.models import Company
 
 
-class CompanySerializer(serializers.HyperlinkedModelSerializer):
+class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
--- authentication/views/simple_add_company.py
+++ authentication/views/simple_add_company.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
+from django.utils.translation import gettext_lazy as _
 
 from ..serializers import CompanySerializer
 from base.serializers import AddressSerializer, ContactSerializer
@@ -15,15 +16,22 @@ class SimpleAddCompanyCreate(CreateModelMixin, GenericAPIView):
         address_data = request.data['address']
         request.data.pop('address', None)
 
-        contact_serializer = ContactSerializer(data=contact_data)
-        contact_serializer.is_valid(raise_exception=True)
-        contact_serializer.save()
+        try:
+            contact_serializer = ContactSerializer(data=contact_data)
+            contact_serializer.is_valid(raise_exception=True)
+            new_contact = contact_serializer.save()
 
-        address_serializer = AddressSerializer(data=address_data)
-        address_serializer.is_valid(raise_exception=True)
-        address_serializer.save()
+            address_serializer = AddressSerializer(data=address_data)
+            address_serializer.is_valid(raise_exception=True)
+            new_address = address_serializer.save()
+
+            request.data['contact'] = new_contact.id
+            request.data['address'] = new_address.id
+
+        except Exception as e:
+            return Response({'error': e})
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response({'msg': 'Mensagem de retorno'})
+        return Response({'success': _('Company created Successfully!')})
